429554df73ae7fa99a9370379009a9bb771bc685
Grocky94 <lastleftdefence@gmail.com>
2023-07-04 01:28:48 +0530

    update

--- tata-cliq/src/components/home/Home.jsx
+++ tata-cliq/src/components/home/Home.jsx
@@ -52,22 +52,316 @@ function Home() {
                 <div className="ondemand-product-child">
                     <div className="ondemand-product-card">
                         <div className="ondemand-product-card-imageHolder">
-                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596857851934.jpg"/>
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596857851934.jpg" />
                         </div>
                     </div>
                     <div className="ondemand-product-card">
                         <div className="ondemand-product-card-imageHolder">
-                        <img src="https://assets.tatacliq.com/medias/sys_master/images/47596865257502.jpg"/>
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596865257502.jpg" />
                         </div>
                     </div>
                     <div className="ondemand-product-card">
                         <div className="ondemand-product-card-imageHolder">
-                        <img src="https://assets.tatacliq.com/medias/sys_master/images/47596857983006.jpg"/>
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596857983006.jpg" />
                         </div>
                     </div>
                     <div className="ondemand-product-card">
                         <div className="ondemand-product-card-imageHolder">
-                        <img src="https://assets.tatacliq.com/medias/sys_master/images/47596866830366.jpg"/>
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596866830366.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/47520344211486.jpg" />
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596650758174.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596853493790.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596853690398.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47596865323038.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/47520344277022.jpg" />
+                </div>
+            </div>
+            <div className="offer-percent-div">
+                <div className="offer-percent-child">
+                    <div className="offer-percent-card">
+                        <div className="offer-percent-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47513577619486.jpg" />
+                        </div>
+                    </div>
+                    <div className="offer-percent-card">
+                        <div className="offer-percent-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47513577685022.jpg" />
+                        </div>
+                    </div>
+                    <div className="offer-percent-card">
+                        <div className="offer-percent-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47513577750558.jpg" />
+                        </div>
+                    </div>
+                    <div className="offer-percent-card">
+                        <div className="offer-percent-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47513577816094.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/47520344342558.jpg" />
+                </div>
+            </div>
+            <div className="left-right-btn-div">
+                <div className="left-right-btn-child-div">
+                    <div className="left-fight-algn-div">
+                        <div className="left-half-round-btn">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div className="right-half-round-btn">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47525840715806.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47525840650270.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47532422168606.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47490926444574.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/45993137373214.jpg" />
+                </div>
+            </div>
+            <div className="large-image-banner-div">
+                <div className="large-image-banner-div-aligning">
+                    <div className="large-image-banner-div-imageHolder">
+                        <img src="https://assets.tatacliq.com/medias/sys_master/images/46904782979102.jpg" />
+                    </div>
+                </div>
+            </div>
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/46863660122142.jpg" />
+                </div>
+            </div>
+            <div className="medium-double-div">
+                <div className="medium-double-div-aligner">
+                    <div className="medium-double-child-div">
+                        <div className="medium-double-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926359527454.jpg"/>
+                        </div>
+                    </div>
+                    <div className="medium-double-child-div">
+                        <div className="medium-double-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926359658526.jpg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="left-right-btn-div">
+                <div className="left-right-btn-child-div">
+                    <div className="left-fight-algn-div">
+                        <div className="left-half-round-btn">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div className="right-half-round-btn">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47314533810206.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46972758655006.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926360248350.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46972759146526.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="left-right-btn-div">
+                <div className="left-right-btn-child-div">
+                    <div className="left-fight-algn-div">
+                        <div className="left-half-round-btn">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div className="right-half-round-btn">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47072148750366.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47627125555230.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47336176386078.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47084689522718.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Mens  */}
+            <div className="deals-wheels-div">
+                <div className="deals-wheels-div">
+                    <img src="https://assets.tatacliq.com/medias/sys_master/images/46863660056606.jpg" />
+                </div>
+            </div>
+            <div className="medium-double-div">
+                <div className="medium-double-div-aligner">
+                    <div className="medium-double-child-div">
+                        <div className="medium-double-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926361198622.jpg"/>
+                        </div>
+                    </div>
+                    <div className="medium-double-child-div">
+                        <div className="medium-double-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926361329694.jpg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="left-right-btn-div">
+                <div className="left-right-btn-child-div">
+                    <div className="left-fight-algn-div">
+                        <div className="left-half-round-btn">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div className="right-half-round-btn">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926361526302.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926361591838.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46926361395230.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/46858377101342.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="left-right-btn-div">
+                <div className="left-right-btn-child-div">
+                    <div className="left-fight-algn-div">
+                        <div className="left-half-round-btn">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </div>
+                        <div className="right-half-round-btn">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="ondemand-product">
+                <div className="ondemand-product-child">
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47627125686302.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47321892421662.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47321892487198.jpg" />
+                        </div>
+                    </div>
+                    <div className="ondemand-product-card">
+                        <div className="ondemand-product-card-imageHolder">
+                            <img src="https://assets.tatacliq.com/medias/sys_master/images/47321892618270.jpg" />
                         </div>
                     </div>
                 </div>
