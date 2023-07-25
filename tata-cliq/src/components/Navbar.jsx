1228690cec1c0427e6ff1e87c3416a14cd73cd2b
Grocky94 <lastleftdefence@gmail.com>
2023-07-25 12:41:39 +0530

    update

--- tata-cliq/src/components/Navbar.jsx
+++ tata-cliq/src/components/Navbar.jsx
@@ -1,14 +1,100 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./../components/Navbar.css"
-import { useNavigate } from "react-router-dom"
-import MultiProductsHomeKitchen from "./multi-products/MultiProductsHomeKitchen";
+import { NavLink, useNavigate } from "react-router-dom"
 function Navbar() {
+    const [log, setLog] = useState(false);
+    const [signup, setsignup] = useState(false);
     const [dropDown, setDropDown] = useState(false);
     const [color, setColor] = useState(false);
     const [backGround, setBackGround] = useState(false);
     const [rotate, setRotate] = useState(false)
     const redirect = useNavigate()
     const [dropDown2, setDropDown2] = useState(false);
+    const [userData, setUserData] = useState({ name: "", email: "", password: "" });
+    const [currentData, setCurrentData] = useState({});
+
+    const handleRegisterInput = (event) => {
+        setUserData({ ...userData, [event.target.name]: event.target.value });
+    }
+
+    const handleRegisterOnsubmit = (event) => {
+        event.preventDefault();
+
+        if (userData.name && userData.email && userData.password) {
+            const user = JSON.parse(localStorage.getItem("Users")) || [];
+            const obj = { name: userData.name, email: userData.email, password: userData.password };
+            user.push(obj);
+            localStorage.setItem("Users", JSON.stringify(user));
+            alert("registration done");
+            setsignup(false);
+        } else {
+            alert("please fill all fields")
+        }
+    }
+
+    const handleLoginInput = (event) => {
+        setUserData({ ...userData, [event.target.name]: event.target.value })
+    }
+
+    const handleLoginOnSubmit = (event) => {
+        event.preventDefault();
+        if (userData.email && userData.password) {
+            const user = JSON.parse(localStorage.getItem("Users"));
+            for (let i = 0; i < user.length; i++) {
+                if (user[i].email == userData.email && user[i].password == userData.password) {
+                    alert("login successful")
+                    localStorage.setItem("Current-User", JSON.stringify(user));
+                    setLog(false);
+                    redirect('/');
+                } else {
+                    alert('credential didnt match')
+                }
+
+            }
+        }
+
+    }
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("Current-User"))
+        const alluser = JSON.parse(localStorage.getItem("Users"));
+        if (user) {
+            for (let i = 0; i < alluser.length; i++) {
+                console.log(alluser[i])
+                if (alluser[i].email === user.email) {
+                    setCurrentData(alluser[i])
+                }
+            }
+
+        }
+    })
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem("Current-User"));
+        //   console.log(user, 'user-here'); 
+        if (user) {
+            setsignup(true);
+        }
+    })
+    function logout() {
+        localStorage.removeItem('Current-User');
+        setsignup(false);
+        alert("logout successful");
+    }
+    function userloginOpen() {
+        setLog(true);
+        const user = JSON.parse(localStorage.getItem("Current-User"));
+        if (!user) {
+            setsignup(true)
+        }
+    }
+    function userloginClose() {
+        setLog(false)
+        const user = JSON.parse(localStorage.getItem("Current-User"));
+        if (!user) {
+            setsignup(false)
+        }
+    }
+
+
 
     function FallDown() {
         setDropDown(true);
@@ -35,37 +121,8 @@ function Navbar() {
     function Profile() {
         redirect('/profile')
     }
-    function Cart() {
-        redirect('/cart')
-    }
-    function Wishlist(){
-        redirect('/wishlist')
-    }
-    function MensMultipleProduct() {
-        redirect("/multiProductsMens ")
-    }
-    function WomensMultipleProduct() {
-        redirect("/multiProductWomens")
-    }
-    function KidsMultipleProduct() {
-        redirect("/multiProductKids")
-    }
-    function HomeKitchenMultiProducts() {
-        redirect('/multiProductHomeKitchen')
-    }
 
-    function BeautyMultipleProduct() {
-        redirect('/multiProductBeauty')
-    }
-    function GadgetsMultipleProduct() {
-        redirect('/multiProductGadgets')
-    }
-    function JewelleryMultipleProduct() {
-        redirect('/multiProductJewellery')
-    }
-    function AccessoriesMultipleProduct() {
-        redirect('/multiProductAccessories')
-    }
+
     return (
         <div id="nav">
             <div id="align-nav">
@@ -81,7 +138,10 @@ function Navbar() {
                         <div className="upper-nav-list">Gift Card</div>
                         <div className="upper-nav-list">CliQ Care</div>
                         <div className="upper-nav-list">Track Order</div>
-                        <div className="upper-nav-list" onMouseEnter={FallDown2} onMouseLeave={Fallup2}>Sign in/ SignUp</div>
+                        {!currentData.name ? (<div className="upper-nav-list" onMouseEnter={FallDown2} onMouseLeave={Fallup2}>Sign in/ SignUp</div>) :
+                            (<div className="upper-nav-list" onMouseEnter={FallDown2} onMouseLeave={Fallup2}>{currentData.name}</div>)}
+
+
                     </div>
                     <div id="below-rightSide">
                         <div className="lower-nav-list" onMouseEnter={FallDown} onMouseLeave={Fallup}>
@@ -97,48 +157,60 @@ function Navbar() {
                             </div>
                         </div>
                         <div className="lower-nav-list">
-                            <div onClick={Wishlist}><i class="fa-regular fa-heart"></i></div>
-                            <div onClick={Cart}><i class="fa-solid fa-bag-shopping" ></i></div>
+                            <NavLink to="/wishlist" className="inner-lower"><i class="fa-regular fa-heart"></i></NavLink>
+                            <NavLink to="/cart" className="inner-lower"><i class="fa-solid fa-bag-shopping" ></i></NavLink>
                         </div>
                     </div>
                 </div>
             </div>
             {dropDown && <div className="category-dropdown" onMouseEnter={FallDown} onMouseLeave={Fallup} style={{ backgroundColor: backGround ? "white" : "black", color: color ? "black" : "white" }}
             >
-                <div className="category-dropdown-cards" onClick={WomensMultipleProduct}>
+                <NavLink to="/multiProductWomens" className="category-dropdown-cards" >
                     <p>Women's Fashion</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={MensMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductsMens" className="category-dropdown-cards">
                     <p>Men's Fashion</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={KidsMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductKids" className="category-dropdown-cards" >
                     <p>Kid's Fashion</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={HomeKitchenMultiProducts}>
+                </NavLink>
+                <NavLink to="/multiProductHomeKitchen" className="category-dropdown-cards" >
                     <p>Home & Kitchen</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={BeautyMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductBeauty" className="category-dropdown-cards" >
                     <p>Beauty</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={GadgetsMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductGadgets" className="category-dropdown-cards" >
                     <p>Gadgets</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={JewelleryMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductJewellery" className="category-dropdown-cards" >
                     <p>Jewellery</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
-                <div className="category-dropdown-cards" onClick={AccessoriesMultipleProduct}>
+                </NavLink>
+                <NavLink to="/multiProductAccessories" className="category-dropdown-cards" >
                     <p>Accessories</p>
                     <i class="fa-solid fa-angle-right"></i>
-                </div>
+                </NavLink>
             </div>}
             {dropDown2 && <div id="profile-side" onMouseEnter={FallDown2} onMouseLeave={Fallup2}>
+                {currentData.name ? (
+                    <div className="profile-side-list">
+                        <div className="profile-side-list-img-hold">
+                            <img src="https://www.tatacliq.com/src/general/components/img/profile.png" />
+                        </div>
+                        <p>{currentData.name}</p>
+                    </div>
+                ) : (
+                    <div className="profile-side-list" onClick={userloginOpen}>
+                        <button id="login-btn">Login/Register</button>
+                    </div>
+                )}
                 <div className="profile-side-list" onClick={Profile}>
                     <div className="profile-side-list-img-hold">
                         <img src="https://www.tatacliq.com/src/general/components/img/profile.png" />
@@ -175,12 +247,37 @@ function Navbar() {
                     </div>
                     <p>CliQ Cash</p>
                 </div>
-                <div className="profile-side-list">
+                <div className="profile-side-list" onClick={logout}>
                     <div className="profile-side-list-img-hold">
                         <img src="https://www.tatacliq.com/src/general/components/img/settingsblack.svg" />
                     </div>
                     <p>LogOut</p>
                 </div>
+            </div>}
+            {log && <div id="parent-signup">
+                {signup && <div id="signup">
+                    <h1>signup</h1><span id="close-x" onClick={userloginClose}>X</span>
+                    <form id="sign-form" onSubmit={handleRegisterOnsubmit}>
+                        <label>Name:</label><br />
+                        <input type="text" name="name" onChange={handleRegisterInput} /><br />
+                        <label>Email:</label><br />
+                        <input type="email" name="email" onChange={handleRegisterInput} /><br />
+                        <label>Password:</label><br />
+                        <input type="password" name="password" onChange={handleRegisterInput} />
+                        <input type="submit" />
+                    </form>
+                </div>}
+                {!signup && <div id="login">
+                    <h1>Login</h1><span id="close-x" onClick={userloginClose}>X</span>
+                    <form id="login-form" onSubmit={handleLoginOnSubmit}>
+                        <label>Email:</label><br />
+                        <input type="email" name="email" onChange={handleLoginInput} /><br />
+                        <label>Password:</label><br />
+                        <input type="password" name="password" onChange={handleLoginInput} />
+                        <input type="submit" />
+                        <p>If not sign-up <button id="click">click here</button></p>
+                    </form>
+                </div>}
             </div>}
         </div>
     )
